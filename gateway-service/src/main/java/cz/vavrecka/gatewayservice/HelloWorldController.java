73c9a4c25e0f62bcef2c56108cf7d97cf347447f
David Vavrecka <david.vavrecka@gmail.com>
2025-08-02 15:08:27 +0200

    Dockerfile, Docker Compose

---
+++ gateway-service/src/main/java/cz/vavrecka/gatewayservice/HelloWorldController.java
@@ -0,0 +1,16 @@
+package cz.vavrecka.gatewayservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloWorldController {
+
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
